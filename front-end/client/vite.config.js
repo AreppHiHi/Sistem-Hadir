4810ca67a38e314ccc0c282f905ddb3086b49cce
AreppHiHi <ariffzakwan64@gmail.com>
2026-05-25 16:00:07 +0800

    Buang plugin basic-ssl untuk Vercel

--- front-end/client/vite.config.js
+++ front-end/client/vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    basicSsl() // Kita pasang semula SSL untuk pujuk iPhone
-  ],
-  server: {
-    host: true,
-    // INI ADALAH JURUS RAHSIA (PROXY)
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Hala ke Backend anda
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+  plugins: [react()],
 })
